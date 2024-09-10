176ab58c80b7ce3acc734cef8165270475ba32d4
Sabyrzhan Rustembekov <rustembekov2003@gmail.com>
2024-09-10 22:07:22 +0500

    Added Firebase Performance

---
+++ FirebasePractice/Performance/PerformanceView.swift
@@ -0,0 +1,19 @@
+//
+//  PerformanceView.swift
+//  FirebasePractice
+//
+//  Created by Sabyrzhan Rustembekov on 10.09.2024.
+//
+
+import SwiftUI
+
+
+struct PerformanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PerformanceView()
+}
